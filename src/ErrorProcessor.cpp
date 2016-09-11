4edb6d8575639ba8f4dfc6b1167618dcef85c117
Anton Todua <toduaanton@yandex.ru>
2016-09-12 00:37:42 +0300

    error highlights implemented

--- src/ErrorProcessor.cpp
+++ src/ErrorProcessor.cpp
@@ -1,5 +1,6 @@
 #include <common.h>
 #include <ErrorProcessor.h>
+#include <Tools.h>
 
 namespace LsplParser {
 
@@ -27,8 +28,38 @@ void CError::Print( ostream& out ) const
 	if( !Line ) {
 		out << "." << endl;
 	} else {
-		out << ": " << endl << Line->Line << endl;
+		const string& source = Line->Line;
 
+		vector<bool> highlightsMask( source.length() + 1 );
+		vector<bool> superHighlightsMask( source.length() + 1 );
+		size_t lashHighlightOffset = 0;
+		for( CLineSegment lineSegment : LineSegments ) {
+			check_logic( lineSegment.Length > 0 );
+			lineSegment.Length = min( lineSegment.Length, source.length() );
+			const size_t first = min( lineSegment.Offset, source.length() );
+			const size_t last = min( first + lineSegment.Length - 1, source.length() );
+			lashHighlightOffset = max( lashHighlightOffset, last );
+			for( size_t i = first; i <= last; i++ ) {
+				highlightsMask[i] = true;
+			}
+			superHighlightsMask[last] = true;
+		}
+		string highlights;
+		highlights.reserve( source.length() + 1 );
+
+		for( size_t i = 0; i <= lashHighlightOffset; i++ ) {
+			char c = i < source.length() ? source[i] : ' ';
+			if( LsplTools::IsByteFirstInUtf8Symbol( c ) ) {
+				if( superHighlightsMask[i] ) {
+					highlights += '^';
+				} else if( highlightsMask[i] ) {
+					highlights += '~';
+				} else {
+					highlights += ' ';
+				}
+			}
+		}
+		out << ": " << endl << source << endl << highlights << endl << endl;
 	}
 }
 
